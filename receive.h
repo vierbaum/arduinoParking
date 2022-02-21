899aabd1c653bbca22970e475d09cf26901b8f0f
nld <nick_daiber@riseup.net>
2022-02-21 17:13:21 +0100

    Mutlithreading

---
+++ receive.h
@@ -0,0 +1,24 @@
+#define INP_PIN 13
+#define OUT_FIN 12
+#define INP_FIN 11
+
+class Receive {
+  void Init() {
+    pinMode(INP_PIN, INPUT);
+    pinMode(OUT_FIN, OUTPUT);
+    pinMode(INP_FIN, INPUT);
+
+    digitalWrite(OUT_FIN, LOW);
+  }
+  
+  int Think() {
+     char* pInp = malloc(17); // major runtime optimization!!!!!!!
+     pInp[17] = 0;
+     for (int i = 0; i < 17; i++) {
+        digitalWrite(OUT_FIN, LOW);
+        while(digitalRead(INP_FIN) == LOW);
+        pInp[i] = digitalRead(INP_PIN);
+        digitalWrite(OUT_FIN, HIGH);
+     }
+  }
+};
---
+++ send.h
@@ -0,0 +1,46 @@
+#define OUT_PIN 13
+#define INP_FIN 12
+#define OUT_FIN 11
+
+class Send {
+  public:
+  void Init() {
+    pinMode(OUT_PIN, OUTPUT);
+    pinMode(INP_FIN, INPUT);
+    pinMode(OUT_FIN, OUTPUT);
+  }
+
+  void Think(int iSendNum) {
+    //binary
+    int a=iSendNum;
+    char* binary = (char*)malloc(17);
+    binary[17] = 0;
+    int mask = 1;
+    for(int i = 16; i != 0; i--) {
+    if((mask&a) >= 1)
+        binary[i] = '1';
+    else
+        binary[i] = '0';
+     mask<<=1;
+    }
+
+    // send number
+    for (int i = 0; i < 17; i++) {
+      // transfer first number anyways
+      if (i == 0) {
+        bool bTransfer = binary[i] == '0' ? false : true;
+        digitalWrite(OUT_PIN, bTransfer);
+        digitalWrite(OUT_FIN, HIGH);
+        continue;
+      }
+
+      // else only transfer when other arduino is ready
+      if (digitalRead(INP_FIN) == HIGH) {
+        digitalWrite(OUT_FIN, LOW);
+        bool bTransfer = binary[i] == '0' ? false : true;
+        digitalWrite(OUT_PIN, bTransfer);
+        digitalWrite(OUT_FIN, HIGH);
+      }
+    }
+  }
+};
