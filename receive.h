729ff6c443d54a87af26bedf7d4a3c5bafa7d3b0
nld <nick_daiber@riseup.net>
2022-03-25 10:10:51 +0100

    not running code

--- receive.h
+++ receive.h
@@ -2,24 +2,31 @@
 #define OUT_FIN 12
 #define INP_FIN 11
 
+#include "Arduino.h"
 class Receive {
+public:
   void Init() {
     pinMode(INP_PIN, INPUT);
     pinMode(OUT_FIN, OUTPUT);
     pinMode(INP_FIN, INPUT);
 
-    digitalWrite(OUT_FIN, LOW);
+    analogWrite(OUT_FIN, LOW);
   }
 
   // trivial
   int Think() {
-     char* pInp = malloc(17); // major runtime optimization!!!!!!!
-     pInp[17] = 0;
-     for (int i = 0; i < 17; i++) {
-        digitalWrite(OUT_FIN, LOW);
-        while(digitalRead(INP_FIN) == LOW);
-        pInp[i] = digitalRead(INP_PIN);
-        digitalWrite(OUT_FIN, HIGH);
+     char* pInp = malloc(33); // minor runtime optimization!!!!!!!
+     pInp[33] = 0;
+     for (int i = 0; i < 33; i++) {
+        while(digitalRead(INP_FIN) == 0){delay(1);};
+        analogWrite(OUT_FIN, LOW);
+        //Serial.print(analogRead(INP_PIN));
+        Serial.print(digitalRead(INP_PIN));
+        //pInp[i] = (char) digitalRead(INP_PIN);
+        analogWrite(OUT_FIN, HIGH);
      }
+
+     //Serial.print(pInp);
+     Serial.println();
   }
 };
--- send.h
+++ send.h
@@ -2,6 +2,7 @@
 #define INP_FIN 12
 #define OUT_FIN 11
 
+#include "Arduino.h"
 class Send {
   public:
   void Init() {
@@ -14,35 +15,28 @@ class Send {
     //binary
     int a=iSendNum;
     // basic char-pointer-array
-    char* binary = (char*)malloc(17);
-    binary[17] = 0;
+    short* binary = (short*)malloc(33);
+    binary[33] = 0;
     int mask = 1;
     // looping through array
-    for(int i = 16; i != 0; i--) {
-    if((mask&a) >= 1)
-        binary[i] = '1';
-    else
-        binary[i] = '0';
-     mask<<=1;
+    for(int i = 32; i != 0; i--) {
+      if((mask&a) >= 1)
+          binary[i] = 255;
+      else
+          binary[i] = 0;
+        mask<<=1;
     }
+    Serial.println();
+
 
     // send number
-    for (int i = 0; i < 17; i++) {
-      // transfer first number anyways
-      if (i == 0) {
-        bool bTransfer = binary[i] == '0' ? false : true;
-        digitalWrite(OUT_PIN, bTransfer);
-        digitalWrite(OUT_FIN, HIGH);
-        continue;
-      }
-
-      // else only transfer when other arduino is ready
-      if (digitalRead(INP_FIN) == HIGH) {
-        digitalWrite(OUT_FIN, LOW);
-        bool bTransfer = binary[i] == '0' ? false : true;
-        digitalWrite(OUT_PIN, bTransfer);
-        digitalWrite(OUT_FIN, HIGH);
-      }
+    for (int i = 0; i < 33; i++) {
+      Serial.println(binary[i]);
+      // transfer when other arduino is ready
+      while(analogRead(INP_FIN) > 200){delay(1);};
+      analogWrite(OUT_FIN, LOW); 
+      analogWrite(OUT_PIN, binary[i]);
+      analogWrite(OUT_FIN, HIGH);
     }
   }
 };
